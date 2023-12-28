5f4bfa0bc4b2b992488b1af50da861f728d915ae
Lucas Fernando Pérez Jorge <62610878+LuFer-GT@users.noreply.github.com>
2023-12-28 17:22:03 -0600

    initial commit

---
+++ HiloSO/src/hiloso/HiloSO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hiloso;
+
+/**
+ *
+ * @author Prof. Marcos Augusto
+ */
+public class HiloSO extends Thread {
+
+  
+    public static void main(String[] args) {
+     System.out.println("Hilo Coriendo");
+    }
+    
+}
